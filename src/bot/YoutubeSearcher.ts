c44e90e5843e83d8a155f786a1bff8224f6a78a2
Hauk Olaussen <olaussen@users.noreply.github.com>
2020-12-02 16:03:18 +0100

    fixed video linking direct

--- src/bot/YoutubeSearcher.ts
+++ src/bot/YoutubeSearcher.ts
@@ -1,6 +1,11 @@
-import yts from "yt-search";
+import yts, { VideoMetadataResult } from "yt-search";
 import { ISong } from "../utils/api";
-import * as db from '../database/DatabaseHandler'
+import * as db from "../database/DatabaseHandler";
+
+function matchYoutubeUrl(url: string) {
+  const ytRegex = /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
+  return url.match(ytRegex);
+}
 
 export const search = async (
   keywords: string,
@@ -11,9 +16,19 @@ export const search = async (
   if (keywords === "" || trynr == 3) {
     return undefined;
   }
-  const obj = await yts(keywords).catch(() => {
-    return undefined;
-  });
+  const matches = matchYoutubeUrl(keywords);
+  let obj: any;
+  if (matches) {
+    const query: string = matches[1];
+    obj = await yts({ videoId: query }).catch(() => {
+      return undefined;
+    });
+    obj = { videos: [obj] };
+  } else {
+    obj = await yts(keywords).catch(() => {
+      return undefined;
+    });
+  }
   if (obj !== undefined && obj.videos.length > 0) {
     const first = obj.videos[0];
     const song: ISong = {
@@ -23,7 +38,7 @@ export const search = async (
       thumbnail: first.thumbnail,
       author: author,
     };
-    db.addNewSong(song, author)
+    db.addNewSong(song, author);
     return song;
   }
   return search(keywords, author, trynr + 1);
