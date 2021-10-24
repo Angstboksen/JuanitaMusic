25cd2a597259dbf1c97665879e2bbddc2d558a43
Hauk Olaussen <olaussen@users.noreply.github.com>
2021-10-24 23:13:56 +0200

    removed juanita ts

--- src/music/JuanitaSubscription.ts
+++ src/music/JuanitaSubscription.ts
@@ -36,51 +36,32 @@ export class JuanitaSubscription {
           newState.reason === VoiceConnectionDisconnectReason.WebSocketClose &&
           newState.closeCode === 4014
         ) {
-          /*
-						If the WebSocket closed with a 4014 code, this means that we should not manually attempt to reconnect,
-						but there is a chance the connection will recover itself if the reason of the disconnect was due to
-						switching voice channels. This is also the same code for the bot being kicked from the voice channel,
-						so we allow 5 seconds to figure out which scenario it is. If the bot has been kicked, we should destroy
-						the voice connection.
-					*/
           try {
             await entersState(
               this.voiceConnection,
               VoiceConnectionStatus.Connecting,
               5_000
             );
-            // Probably moved voice channel
           } catch {
             this.voiceConnection.destroy();
-            // Probably removed from voice channel
           }
         } else if (this.voiceConnection.rejoinAttempts < 5) {
-          /*
-						The disconnect in this case is recoverable, and we also have <5 repeated attempts so we will reconnect.
-					*/
+   
           await wait((this.voiceConnection.rejoinAttempts + 1) * 5_000);
           this.voiceConnection.rejoin();
         } else {
-          /*
-						The disconnect in this case may be recoverable, but we have no more remaining attempts - destroy.
-					*/
+      
           this.voiceConnection.destroy();
         }
       } else if (newState.status === VoiceConnectionStatus.Destroyed) {
-        /*
-					Once destroyed, stop the subscription
-				*/
+      
         this.stop();
       } else if (
         !this.readyLock &&
         (newState.status === VoiceConnectionStatus.Connecting ||
           newState.status === VoiceConnectionStatus.Signalling)
       ) {
-        /*
-					In the Signalling or Connecting states, we set a 20 second time limit for the connection to become ready
-					before destroying the voice connection. This stops the voice connection permanently existing in one of these
-					states.
-				*/
+    
         this.readyLock = true;
         try {
           await entersState(
@@ -100,18 +81,17 @@ export class JuanitaSubscription {
       }
     });
 
-    // Configure audio player
+
     this.audioPlayer.on("stateChange", (oldState, newState) => {
       if (
         newState.status === AudioPlayerStatus.Idle &&
         oldState.status !== AudioPlayerStatus.Idle
       ) {
-        // If the Idle state is entered from a non-Idle state, it means that an audio resource has finished playing.
-        // The queue is then processed to start playing the next track, if one is available.
+
         (oldState.resource as AudioResource<Track>).metadata.onFinish();
         void this.processQueue();
       } else if (newState.status === AudioPlayerStatus.Playing) {
-        // If the Playing state has been entered, then a new track has started playback.
+
         (newState.resource as AudioResource<Track>).metadata.onStart();
       }
     });
@@ -123,12 +103,6 @@ export class JuanitaSubscription {
     voiceConnection.subscribe(this.audioPlayer);
   }
 
-  /**
-   * Adds a new Track to the queue.
-   *
-   * @param track The track to add to the queue
-   * @param first Whether or not to prioritize the track
-   */
   public enqueue(track: Track, first: boolean = false) {
     if (first) this.queue = [track].concat(this.queue);
     else this.queue.push(track);
@@ -147,20 +121,14 @@ export class JuanitaSubscription {
     }
   };
 
-  /**
-   * Stops audio playback and empties the queue
-   */
   public stop() {
     this.queueLock = true;
     this.queue = [];
     this.audioPlayer.stop(true);
   }
 
-  /**
-   * Attempts to play a Track from the queue
-   */
+
   private async processQueue(): Promise<void> {
-    // If the queue is locked (already being processed), is empty, or the audio player is already playing something, return
     if (
       this.queueLock ||
       this.audioPlayer.state.status !== AudioPlayerStatus.Idle ||
@@ -168,13 +136,13 @@ export class JuanitaSubscription {
     ) {
       return;
     }
-    // Lock the queue to guarantee safe access
+
     this.queueLock = true;
 
-    // Take the first item from the queue. This is guaranteed to exist due to the non-empty check above.
+
     const nextTrack = this.queue.shift()!;
     try {
-      // Attempt to convert the Track into an AudioResource (i.e. start streaming the video)
+
       const { title, requestor, isSpotify } = nextTrack.song;
       if (isSpotify) {
         const song = await YTSearcher.search(title, requestor);
@@ -185,7 +153,6 @@ export class JuanitaSubscription {
       this.audioPlayer.play(resource);
       this.queueLock = false;
     } catch (error) {
-      // If an error occurred, try the next item of the queue instead
       nextTrack.onError(error as Error);
       this.queueLock = false;
       return this.processQueue();
