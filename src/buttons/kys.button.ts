d224a986a214763065d38702814ea9e31819c9a7
Hauk Aleksander Olaussen <olaussen.hauk@gmail.com>
2023-01-23 16:33:58 +0100

    ✅Translated buttons

---
+++ src/buttons/kys.button.ts
@@ -0,0 +1,17 @@
+import SimpleEmbed, { EmbedType } from '../embeds/embeds';
+import { GENERIC_ERROR, KYS_SUCCESS } from '../embeds/messages';
+import type { JuanitaButtonOptions } from './types';
+
+export default async ({ interaction, queue, juanitaGuild }: JuanitaButtonOptions) => {
+	if (!queue || !queue.current)
+		return interaction.reply({
+			embeds: [SimpleEmbed(GENERIC_ERROR[juanitaGuild.lang], EmbedType.Error)],
+			ephemeral: true,
+		});
+
+	queue.destroy();
+
+	return interaction.reply({
+		embeds: [SimpleEmbed(KYS_SUCCESS[juanitaGuild.lang], EmbedType.Success)],
+	});
+};
