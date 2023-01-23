12143921a1ebc6a64223b82f5e38123ae80c82a5
Hauk Aleksander Olaussen <olaussen.hauk@gmail.com>
2023-01-23 16:52:13 +0100

    ✅Create next and previous buttons

---
+++ src/buttons/next.button.ts
@@ -0,0 +1,20 @@
+import SimpleEmbed, { EmbedType } from '../embeds/embeds';
+import { GENERIC_ERROR, QUEUE_NEXT_PAGE_ERROR } from '../embeds/messages';
+import type { JuanitaButtonOptions } from './types';
+
+export default async ({ interaction, queue, juanitaGuild }: JuanitaButtonOptions) => {
+	if (!queue || !queue.current)
+		return interaction.reply({
+			embeds: [SimpleEmbed(GENERIC_ERROR[juanitaGuild.lang], EmbedType.Error)],
+			ephemeral: true,
+		});
+
+	const success = juanitaGuild.nextQueuePage();
+	if (!success)
+		return interaction.reply({
+			embeds: [SimpleEmbed(QUEUE_NEXT_PAGE_ERROR[juanitaGuild.lang], EmbedType.Error)],
+			ephemeral: true,
+		});
+
+	return interaction.deferUpdate();
+};
