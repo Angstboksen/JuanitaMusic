55c06ef48d998357e5196581846614207d1e9eab
Hauk Aleksander Olaussen <olaussen.hauk@gmail.com>
2023-02-14 22:13:31 +0100

    regex for ? in url

--- src/utils/spotify.ts
+++ src/utils/spotify.ts
@@ -11,8 +11,13 @@ export const validateSpotifyURI = async (uri: string): Promise<string> => {
 };
 
 export const retrieveSpotifyPlaylistId = (uri: string): string | null => {
-	const spotifyRegex = /(?:spotify:playlist:|https:\/\/open\.spotify\.com\/playlist\/)([a-zA-Z0-9]+)/;
-	const match = uri.match(spotifyRegex);
-	return match ? match[1]! : null;
-
+	// parse out playlistid in spotify uri and url when url looks likehttps://open.spotify.com/playlist/4BGFC3yiyQVNTDeMDirTfl?si=0a0a0a0a0a0a0a0a
+	// and uri looks like spotify:playlist:4BGFC3yiyQVNTDeMDirTfl
+	const regex = /spotify:playlist:([a-zA-Z0-9]+)/;
+	const regex2 = /open.spotify.com\/playlist\/([a-zA-Z0-9]+)/;
+	const match = uri.match(regex);
+	const match2 = uri.match(regex2);
+	if (match) return match[1];
+	if (match2) return match2[1];
+	return null;
 };
