3d82de54a95a50153a34fabbac9f65193a8b736e
Olaussen <olaussen.hauk@gmail.com>
2023-06-13 17:11:51 +0200

    feat: rebase python

--- src/events/ready.ts
+++
@@ -1,9 +0,0 @@
-import { ActivityType } from "discord.js";
-import type JuanitaClient from "../JuanitaClient";
-
-export default async (client: JuanitaClient) => {
-	console.log(
-		`Logged to the client ${client.user?.username}\n-> Ready on ${client.guilds.cache.size} servers for a total of ${client.users.cache.size} users`,
-	);
-	client.user?.setActivity(client.config.app.playing, { type: ActivityType.Listening });
-};
